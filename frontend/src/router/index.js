a6cd6b48200d71da5446f1674675fcc2a29e5ecd
Zhmzjy <161552291+Zhmzjy@users.noreply.github.com>
2025-08-21 22:29:44 +1000

    setup Vue app core structure with routing and global styles
    setup Vue app core structure with routing and global styles

---
+++ frontend/src/router/index.js
@@ -0,0 +1,33 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import('../pages/Home.vue')
+  },
+  {
+    path: '/learn-wildlife',
+    name: 'LearnWildlife',
+    component: () => import('../pages/LearnWildlife.vue')
+  },
+  {
+    path: '/find-zoo',
+    name: 'FindZoo',
+    component: () => import('../pages/FindZoo.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { top: 0 }
+    }
+  }
+})
+
+export default router
